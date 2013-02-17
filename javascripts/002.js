eb13405aff7b80e0b429b5674824b5daa20de300
Fecc <gulyasfeccferenc@gmail.com>
2013-02-17 15:36:24 +0100

    Update javascripts/002.js

--- javascripts/002.js
+++ javascripts/002.js
@@ -3,14 +3,14 @@ var myNamespacedSolution = {
 
   forras:("images/01.jpg","images/02.jpg","images/03.jpg","images/04.jpg","images/05.jpg"),
   
-  onClick: function {
+  onClick: function() {
     ++myNamespacedSolution.szamlalo;
     if(myNamespacedSolution.szamlalo<myNamespacedSolution.forras.length){
       kep.src=forras[szamlalo]; }
     else {myNamespacedSolution.szamlalo=0; myNamespacedSolution.kep.src=myNamespacedSolution.forras[myNamespacedSolution.szamlalo];},
   },
 
-  function setUp() {
+  setUp: function() {
     myNamespacedSolution.kep = document.getElementById('kephely');
     myNamespacedSolution.kep.addEventListener('click', myNamespacedSolution.onClick);
   }
