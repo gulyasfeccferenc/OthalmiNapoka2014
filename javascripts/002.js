f2b21018ce926286a30910dc45833cc494deef41
Fecc <gulyasfeccferenc@gmail.com>
2013-02-17 06:20:50 -0800

    Create 002.js

---
+++ javascripts/002.js
@@ -0,0 +1,20 @@
+var myNamespacedSolution = {
+  var szamlalo;
+  szamlalo=0;
+
+  var forras=new Array("images/01.jpg","images/02.jpg","images/03.jpg","images/04.jpg","images/05.jpg");
+  
+  function onClick(ev) {
+    ++szamlalo;
+    if(szamlalo<forras.length){
+      kep.src=forras[szamlalo];}
+    else {szamlalo=0;kep.src=forras[szamlalo];}
+  }
+
+  function setUp() {
+    kep = document.getElementById('kephely');
+    kep.addEventListener('click', onClick);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', myNamespacedSolution.setUp);
