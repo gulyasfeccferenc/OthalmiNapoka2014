39fd19f46e6152ec3c5b694ebc9f7c93f63a5cef
Fecc <gulyasfeccferenc@gmail.com>
2013-02-17 15:33:08 +0100

    Update javascripts/002.js

--- javascripts/002.js
+++ javascripts/002.js
@@ -1,18 +1,18 @@
 var myNamespacedSolution = {
   szamlalo:0,
 
-  forras:("images/01.jpg","images/02.jpg","images/03.jpg","images/04.jpg","images/05.jpg");
+  forras:("images/01.jpg","images/02.jpg","images/03.jpg","images/04.jpg","images/05.jpg"),
   
   onClick: function {
-    ++myNamespacedSolution.szamlalo,
+    ++myNamespacedSolution.szamlalo;
     if(myNamespacedSolution.szamlalo<myNamespacedSolution.forras.length){
-      kep.src=forras[szamlalo] }
-    else {myNamespacedSolution.szamlalo=0,myNamespacedSolution.kep.src=myNamespacedSolution.forras[myNamespacedSolution.szamlalo];}
-  }
+      kep.src=forras[szamlalo]; }
+    else {myNamespacedSolution.szamlalo=0; myNamespacedSolution.kep.src=myNamespacedSolution.forras[myNamespacedSolution.szamlalo];},
+  },
 
   function setUp() {
-    myNamespacedSolution.kep = document.getElementById('kephely'),
-    myNamespacedSolution.kep.addEventListener('click', myNamespacedSolution.onClick)
+    myNamespacedSolution.kep = document.getElementById('kephely');
+    myNamespacedSolution.kep.addEventListener('click', myNamespacedSolution.onClick);
   }
 }
 
