67080c9decc846d066371589b2dead32e260df4d
Fecc <gulyasfeccferenc@gmail.com>
2013-02-24 22:44:31 +0100

    Update javascripts/jasmintest1.js

--- javascripts/jasmintest1.js
+++ javascripts/jasmintest1.js
@@ -2,7 +2,7 @@ describe("Algoritmustesztelés:", function() {
  it("FizzBuzzBizz értékekre", function() {
    expect(true).toBe(true);
    expect(true).toBe(true);
-   expect(true).toBe(true);
+   expect(true).not.toBe(true);
    expect(true).toBe(true);
    expect(true).toBe(true);
  });
