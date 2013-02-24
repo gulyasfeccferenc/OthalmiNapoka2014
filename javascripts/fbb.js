b40683c15434a75b759275635afe586c02f48b84
Fecc <gulyasfeccferenc@gmail.com>
2013-02-24 17:05:03 +0100

    Update javascripts/fbb.js

--- javascripts/fbb.js
+++ javascripts/fbb.js
@@ -1,5 +1,22 @@
-var liContainer; 
+var outputContainer; 
 var counter;
+var atm;
 
 function listCounter(msg) {
     var li = document.createElement('li');
+    li.innerHTML = msg;
+    outputContainer.appendChild(li);
+}
+
+function fizz() {
+    outputContainer = document.getElementById('kimenet');
+    for (var c = 1; c <=100; c++) {
+    atm = "";
+    if (c % 3 == 0) {atm = atm+"Fizz"};
+    listCounter(atm);
+}
+
+}
+
+
+document.addEventListener('DOMContentLoaded', fizz);
