137fc5d77d180d2b6e5d98c37ba5f8c7f3ea3610
Fecc <gulyasfeccferenc@gmail.com>
2013-02-24 22:57:07 +0100

    Update javascripts/jasmintest1.js

--- javascripts/jasmintest1.js
+++ javascripts/jasmintest1.js
@@ -1,9 +1,9 @@
 describe("Algoritmustesztelés:", function() {
  it("FizzBuzzBizz értékekre", function() {
    expect(myFizzy.fizz(3)).toBe("Fizz");
-   expect(true).toBe(true);
-   expect(true).toBe(true);
-   expect(true).toBe(true);
-   expect(true).toBe(true);
+   expect(myFizzy.fizz(5)).toBe("Buzz");
+   expect(myFizzy.fizz(7)).toBe("Bizz");
+   expect(myFizzy.fizz(105)).toBe("FizzBuzzBizz");
+   expect(myFizzy.fizz(-1)).toBe("");
  });
 });
