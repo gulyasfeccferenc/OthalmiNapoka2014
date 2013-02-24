4cf8c55232522fc0c6a4e2f00787802c8b1ee0e1
Fecc <gulyasfeccferenc@gmail.com>
2013-02-24 22:18:09 +0100

    Update javascripts/jasmintest1.js

--- javascripts/jasmintest1.js
+++ javascripts/jasmintest1.js
@@ -1,1 +1,6 @@
-jasmintest1
+ describe("FizzBuzzBizz algoritmus működésének tesztelése Jasminnal", function() {
+          it("rendben zajlott.", function() {
+            alert("msg");
+            expect(myFizz.fizz(3)).toBe("Fizz");
+          });
+          });
